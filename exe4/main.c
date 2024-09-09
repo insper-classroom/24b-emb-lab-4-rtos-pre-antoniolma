01ff72df272bb370382fd2a92e3a310acae02dc9
antoniolma <antoniolucasmda@gmail.com>
2024-09-09 15:47:25 -0300

    ex4-done v1.1

--- exe4/main.c
+++ exe4/main.c
@@ -13,7 +13,6 @@ const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
 QueueHandle_t xQueueButId;
-QueueHandle_t xQueueButG;
 SemaphoreHandle_t xSemaphore_g;
 SemaphoreHandle_t xSemaphore_r;
 
@@ -22,9 +21,10 @@ void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
         if (gpio == BTN_PIN_R) {
             xSemaphoreGiveFromISR(xSemaphore_r, 0);
-        }else if (gpio == BTN_PIN_G) {
+        } else if (gpio == BTN_PIN_G) {
             xSemaphoreGiveFromISR(xSemaphore_g, 0);
         }
+        xQueueSendFromISR(xQueueButId, &gpio, 0);
     }
 }
 
@@ -56,7 +56,7 @@ void led_2_task(void *p) {
 
     int delay = 0;
     while (true) {
-        if (xQueueSend(xQueueButG, &delay, 0)) {
+        if (xQueueSend(xQueueButId, &delay, 0)) {
             printf("g: %d\n", delay);
         }
 
@@ -89,18 +89,6 @@ void btn_1_task(void *p) {
             printf("delay btn r %d \n", delay);
             xQueueSend(xQueueButId, &delay, 0);
         }
-    }
-}
-
-void btn_2_task(void *p) {
-    gpio_init(BTN_PIN_G);
-    gpio_set_dir(BTN_PIN_G, GPIO_IN);
-    gpio_pull_up(BTN_PIN_G);
-    gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
-                                        &btn_callback);
-
-    int delay = 0;
-    while (true) {
         if (xSemaphoreTake(xSemaphore_g, pdMS_TO_TICKS(500)) == pdTRUE) {
             if (delay < 1000) {
                 delay += 100;
@@ -108,7 +96,7 @@ void btn_2_task(void *p) {
                 delay = 100;
             }
             printf("delay btn g %d \n", delay);
-            xQueueSend(xQueueButG, &delay, 0);
+            xQueueSend(xQueueButId, &delay, 0);
         }
     }
 }
@@ -119,16 +107,13 @@ int main() {
     printf("Start RTOS \n");
 
     xQueueButId = xQueueCreate(32, sizeof(int));
-    xQueueButG = xQueueCreate(32, sizeof(int));
 
     xSemaphore_r = xSemaphoreCreateBinary();
     xSemaphore_g = xSemaphoreCreateBinary();
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
-    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
-    
     xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
-    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
